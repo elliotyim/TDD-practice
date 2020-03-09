2745516e89543f20cd5c9485aa8f580d32796ea4
yimsb01 <yimsb02@naver.com>
2020-03-09 14:02:48 +0900

    Ch.10 - 흥미로운 시간

--- src/main/java/Money.java
+++ src/main/java/Money.java
@@ -1,8 +1,6 @@
-public abstract class Money {
-    abstract Money times(int multiplier);
-
+public class Money {
     protected int amount;
-    protected  String currency;
+    protected String currency;
 
     static Money dollar(int amount) {
         return new Dollar(amount, "USD");
@@ -21,10 +19,19 @@ public abstract class Money {
         return currency;
     }
 
+    Money times(int multiplier) {
+        return new Money(amount * multiplier, currency);
+    }
+
     @Override
     public boolean equals(Object object) {
         Money money = (Money) object;
         return this.amount == money.amount
-                && getClass().equals(money.getClass());
+                && currency().equals(money.currency());
+    }
+
+    @Override
+    public String toString() {
+        return amount + " " + currency;
     }
 }
