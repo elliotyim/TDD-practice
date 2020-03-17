6aa958aa1d662bdaefb3a7cf793f2b191b414978
yimsb01 <yimsb02@naver.com>
2020-03-17 10:34:55 +0900

    Ch.16 - 드디어, 추상화

--- src/main/java/Money.java
+++ src/main/java/Money.java
@@ -19,7 +19,7 @@ public class Money implements Expression {
         return currency;
     }
 
-    Expression times(int multiplier) {
+    public Expression times(int multiplier) {
         return new Money(amount * multiplier, currency);
     }
 
