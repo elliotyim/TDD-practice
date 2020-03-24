26d2b0657503a6e6fc594bd25e201a6940c2f238
yimsb01 <yimsb02@naver.com>
2020-03-24 12:51:01 +0900

    Ch.20 - 뒷정리하기

--- python/test.py
+++ python/test.py
@@ -5,16 +5,12 @@ class TestCaseTest(TestCase):
     def set_up(self):
         self.test = WasRun("test_method")
 
-    def test_running(self):
-        self.test.run()
-        assert self.test.was_run
-
-    def test_set_up(self):
-        self.test.run()
-        assert self.test.was_set_up
+    def test_template_method(self):
+        test = WasRun("test_method")
+        test.run()
+        assert "set_up test_method tear_down " == test.log
 
 
 if __name__ == '__main__':
-    TestCaseTest("test_running").run()
-    TestCaseTest("test_set_up").run()
+    TestCaseTest("test_template_method").run()
 
